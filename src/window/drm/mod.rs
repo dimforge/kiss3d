f6773b876acc70a0b8d482d4a1bd9c7b6904552c
Florian Limburg <florian.limburg@actian.com>
2026-03-21 19:41:16 +0100

    step 6

--- src/window/drm/mod.rs
+++ src/window/drm/mod.rs
@@ -1,10 +1,8 @@
 mod card;
 mod display_thread;
 mod drm_canvas;
-mod drm_canvas_wrapper;
 mod drm_events;
 mod drm_window;
 
 pub use drm_canvas::DrmCanvas;
-pub use drm_canvas_wrapper::DrmCanvasWrapper;
 pub use drm_window::Window;
